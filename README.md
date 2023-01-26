99fb75af20d25e3c6e5522bd0230dd9c8b8f0c59
alan <crwu0206@gmail.com>
2023-01-26 22:20:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## About
+使用 2captcha 服务，实现破解图形验证码并自动登入博客来网站
+
+## Feature
+
+* 使用 2captcha 服务, 识别并绕过图形验证码
+* 支持其他类型验证码, 例 reCAPTCHA V2/V3、hCaptcha、KeyCaptcha、GeeTest等
+
+## Requires
+Python 3.11.0  
+selenium 4.8.0  
+webdriver_manager 3.8.5   
+configparser 5.3.0  
+
+## Usage
+```
+python main.py
+```
+![image](https://raw.githubusercontent.com/joanbabyfet/md_img/master/crack_captcha/display.jpg)
+
+## Change Log
+v1.0.0
+
+## Maintainers
+Alan
+
+## LICENSE
+[MIT License](https://github.com/joanbabyfet/crack_captcha/blob/master/LICENSE)
---
+++ main.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+import configparser
+import base64
+import requests
+
+def main():
+    url = 'https://cart.books.com.tw/member/login'
+
+    conf = configparser.ConfigParser()
+    conf.read('config.ini', encoding='utf-8') # 这里要加utf-8, 否则会报错, 默认gbk
+    config_section  = 'config'
+    username = conf.get(config_section, 'username') # 帐号
+    password = conf.get(config_section, 'password') # 密码
+    api_key = conf.get(config_section, 'api_key') 
+
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)  # 不自动关闭浏览器
+    options.add_experimental_option('prefs', { 
+        "profile.default_content_setting_values.notifications": 2 # 防止跳出通知
+    })
+    browser = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options = options)
+    browser.maximize_window() # 窗口最大化
+    browser.get(url)
+
+    # toDataURL将内容转成图片, canvas是h5标签的容器, 可在里面绘图
+    img_base64 = browser.execute_script('''
+        var el = arguments[0];
+        var canvas = document.createElement('canvas');
+        canvas.width = el.width;
+        canvas.height = el.height;
+        canvas.getContext('2d').drawImage(el, 0, 0);
+        return canvas.toDataURL('image/jpeg').substring(22);
+    ''', browser.find_element(By.XPATH, "//*[@id='captcha_img']/img"))
+    with open('captcha_login.png', 'wb') as image:
+        image.write(base64.b64decode(img_base64))
+
+    # 下载该验证码图片
+    file = {'file': open('captcha_login.png', 'rb')} 
+
+    data = {
+        'key': api_key,
+        'method': 'post'
+    }
+    # 验证码提交地址
+    res = requests.post('http://2captcha.com/in.php', files = file, data = data)
+    print(f'response:{res.text}')
+    if res.ok and res.text.find('OK') > -1:
+        captcha_id = res.text.split('|')[1]
+        for i in range(10):
+            # 获取辨识结果
+            resp = requests.get(f'http://2captcha.com/res.php?key={api_key}&action=get&id={captcha_id}')
+            print(f'response:{resp.text}')
+
+            if resp.text.find('OK') > -1:
+                captcha_text = resp.text.split('|')[1] # 获取辨识结果
+                print('辨识结果:', captcha_text)
+
+                el_username = browser.find_element(By.ID, 'login_id')
+                el_password = browser.find_element(By.ID, 'login_pswd')
+                el_captcha = browser.find_element(By.ID, 'captcha')
+                btn_login = browser.find_element(By.ID, 'books_login')
+                el_username.send_keys(username)  
+                el_password.send_keys(password) 
+                el_captcha.send_keys(captcha_text)
+                btn_login.click()  # 点击登入
+                break
+            elif resp.text.find('CAPCHA_NOT_READY') > -1: # 未辨识完成
+                sleep(3)
+        else:
+            print('获取验证码错误')
+    else:
+        print('提交验证码错误')
+
+if __name__ == '__main__':
+    main()
